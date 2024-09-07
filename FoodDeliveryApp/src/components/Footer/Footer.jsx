ca906845ee123a69d53f84a4d5c57f404b25f7a4
prabhakaranwebdeveloper <prabhakaranwebdeveloper@gmail,com>
2024-09-07 12:33:06 +0530

    first commit

---
+++ FoodDeliveryApp/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Footer.css'
+import {assets} from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+    <div className="footer-content">
+        <div className="footer-content-left">
+        <img src={assets.logo} alt="" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque iusto excepturi repellat saepe voluptate quo, quisquam dolorem tempore error quis nemo dignissimos quasi magni! Quo rerum error exercitationem tempora tempore.</p>
+        <div className="footer-social-icons">
+         <img src={assets.facebook_icon} alt="" />
+         <img src={assets.twitter_icon} alt="" />
+         <img src={assets.linkedin_icon} alt="" />
+        </div>
+        </div>
+
+        <div className="footer-content-center">
+        <h2>COMPANY</h2>
+        <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+        </ul>
+        </div>
+
+        <div className="footer-content-right">
+        <h2>GET IN TOUCH</h2>
+        <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+        </ul>
+        </div>
+        
+    </div>
+    <hr />
+    <p className="footer-copyrights">Copyright 2024 © Tomato.com - All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
