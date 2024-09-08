d5fbfd40a1c8d6bec8308777ab1811ea8f6a0185
prabhakaranwebdeveloper <prabhakaranwebdeveloper@gmail,com>
2024-09-08 13:25:17 +0530

    first commit

--- FoodDeliveryApp/vite.config.js
+++ FoodDeliveryApp/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Food-Delivery-App/"
-});
+  plugins: [react()]
+})
